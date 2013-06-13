5a55066ef6fe1c70252cfd2dfc2b33292b5dddd4
Jake Levine <jake@drunkonvinyl.com>
2013-06-12 19:10:47 -0700

    Block access to user_stories for logged out users

--- app/controllers/user_stories_controller.rb
+++ app/controllers/user_stories_controller.rb
@@ -1,2 +1,24 @@
 class UserStoriesController < InheritedResources::Base
+  before_filter :authenticate_user!
+
+  # def new
+  # end
+
+  # def create
+  # end
+
+  # def update
+  # end
+
+  # def edit
+  # end
+
+  # def destroy
+  # end
+
+  # def index
+  # end
+
+  # def show
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Aop::Application.routes.draw do
+  get "user_stories/new"
+
+  get "user_stories/create"
+
+  get "user_stories/update"
+
+  get "user_stories/edit"
+
+  get "user_stories/destroy"
+
+  get "user_stories/index"
+
+  get "user_stories/show"
+
   resources :user_stories
 
   devise_for :admin_users, ActiveAdmin::Devise.config
